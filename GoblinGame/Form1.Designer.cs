47131791ba60af806738a3a79f310ce7d7354d78
Kyja McCabe <kyjamccabe@mmc.school.nz>
2020-08-03 11:00:28 +1200

    3/08

--- GoblinGame/Form1.Designer.cs
+++ GoblinGame/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GoblinGame
 {
-    partial class Form1
+    partial class FrmGame
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmGame";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- GoblinGame/Form1.cs
+++ GoblinGame/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GoblinGame
 {
-    public partial class Form1 : Form
+    public partial class FrmGame : Form
     {
-        public Form1()
+        public FrmGame()
         {
             InitializeComponent();
         }
